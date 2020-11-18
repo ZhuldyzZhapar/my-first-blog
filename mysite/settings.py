56531a18df97590075b6d7efd5f1fe10baaeae29
ZhuldyzZhapar <starzhuldyz01@gmail.com>
2020-11-18 18:32:08 +0600

    blabla

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b47ou)1)5n+(3sz^s_kyan4rnuxs!fe51l2s0v(10(!3c7+0!9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
